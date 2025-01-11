12a64bc564358f19d653816ad97749da0cd04912
Graham Maher <mahergrahamsj@gmail.com>
2025-01-11 18:54:52 +0000

    Add models to restaurant_booking models.py for table and for reservation

--- restaurant_booking/models.py
+++ restaurant_booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Table(models.Model):
+    number = models.IntegerField(unique=True)
+    seats = models.IntegerField()
+
+
+class Reservation(models.Models):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    date_time = models.DateTimeField()
+    guests = models.IntegerField()
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
