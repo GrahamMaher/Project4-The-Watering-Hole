55a94f7bb6eca8e6a8dde67df1797596035ec93c
Graham Maher <mahergrahamsj@gmail.com>
2025-01-11 21:15:47 +0000

    Add return values to the table and reservation models

--- restaurant_booking/models.py
+++ restaurant_booking/models.py
@@ -5,6 +5,9 @@ class Table(models.Model):
     number = models.IntegerField(unique=True)
     seats = models.IntegerField()
 
+    def __str__(self):
+        return f'Table {self.number} - Seats: {self.seats}'
+
 
 class Reservation(models.Models):
     name = models.CharField(max_length=50)
@@ -12,3 +15,6 @@ class Reservation(models.Models):
     date_time = models.DateTimeField()
     guests = models.IntegerField()
     table = models.ForeignKey(Table, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Reservation for {self.name} on {self.date_time}'
